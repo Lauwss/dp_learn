5b98342783315cb400ab74fc91bddf58c86de4b3
Lauss <2034282901@qq.com>
2025-11-20 22:41:20 +0800

    更新了tool中的regression函数,增加是否能考虑偏置这一训练模式

--- utils/tool.py
+++ utils/tool.py
@@ -1,6 +1,6 @@
 import torch
 import torchvision
-from torch.utils import data
+from torch.utils import data as Data
 from torchvision import transforms
 from d2l import torch as d2l
 from tqdm import tqdm
@@ -87,18 +87,20 @@ def evaluate_loss(net, data_iter, loss):  #@save
         metric.add(l.sum(), l.numel())
     return metric[0] / metric[1]
 
-def train_regression(learning_rate,train_features, test_features, train_labels, test_labels,
-          num_epochs=400):
-    loss = nn.MSELoss(reduction='none')
+def train_regression(learning_rate,train_features, test_features, train_labels, test_labels,num_epochs=400,is_bias=True):
+    loss = nn.MSELoss()
     input_shape = train_features.shape[-1]
-    # 不设置偏置，因为我们已经在多项式中实现了它
-    net = nn.Sequential(nn.Linear(input_shape, 1, bias=False))
+    net = nn.Sequential(nn.Linear(input_shape, 1, bias=is_bias))
+    
+    net[0].weight.data.normal_(0, 0.01)
+    if is_bias:
+     net[0].bias.data.fill_(0)
     batch_size = min(10, train_labels.shape[0])
     train_iter = d2l.load_array((train_features, train_labels.reshape(-1,1)),
                                 batch_size)
     test_iter = d2l.load_array((test_features, test_labels.reshape(-1,1)),
                                batch_size, is_train=False)
-    trainer = torch.optim.SGD(net.parameters(), lr=learning_rate)
+    trainer = torch.optim.Adam(net.parameters(), lr=learning_rate)
     animator = d2l.Animator(xlabel='epoch', ylabel='loss', yscale='log',
                             xlim=[1, num_epochs], ylim=[1e-3, 1e2],
                             legend=['train', 'test'])
